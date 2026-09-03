fcb560fd6daf49b273d8c3ea293ec4127f520e6b
husni-sanlabz <husni@sanlabz.com>
2026-09-03 23:50:04 +0530

    Allows special characters

---
+++ tests/verify_special_character_handling.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+import { loginToApp } from './login.spec.js';
+
+test('Verify subject creation with special characters', async ({ page }) => {
+
+    // Login to the application
+    await loginToApp(page);
+
+    // Verify dashboard page
+    await page.locator('div').filter({ hasText: /^Dashboard$/ }).click();
+
+    await expect(page.getByText('Dashboard', { exact: true })).toBeVisible();
+
+    // Navigate to Subject Management
+    await page.locator('div').filter({ hasText: /^Master Data$/ }).click();
+    await page.getByText('Subject Management').click();
+   
+    await page.getByRole('button', { name: 'New Subject' }).click();
+    await page.getByRole('textbox', { name: 'Enter subject name' }).fill('Special!@#$%^&*()_+Characters');
+    await page.getByLabel('Add Subject').locator('span').filter({ hasText: 'Active' }).click();
+    await page.getByRole('option', { name: 'Active', exact: true }).click();
+    await page.getByRole('textbox', { name: 'Enter description' }).fill('JavaScript is a versatile programming language used for web development, enabling interactive and dynamic content on websites.');
+    await page.getByRole('button', { name: 'Save' }).click();
+    
+    const toast = page.getByRole('alert');
+    await expect(toast.getByText('Subject created successfully')).toBeVisible();
+    await expect(toast.getByText('Success', { exact: true })).toBeVisible();
+
+     // Close success message
+    await page.getByRole('alert').getByRole('button').click();
+
+    // Wait for subject management page to load after subject creation
+    await page.waitForURL('**/subject-management');
+    await expect(page).toHaveURL(/subject-management/);
+
+})
